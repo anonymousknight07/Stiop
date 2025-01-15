539151f420746be5171c821f34873d6c2190a007
Akshath Pandey <21951a0504@iare.ac.in>
2025-01-16 00:54:08 +0530

    fixed the notes

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import  { useState, useEffect } from 'react';
 import { Search, Plus, Trash2, Bold, Italic, List, Link, ArrowUp, ArrowDown } from 'lucide-react';
 import { marked } from 'marked';
 import DOMPurify from 'dompurify';
